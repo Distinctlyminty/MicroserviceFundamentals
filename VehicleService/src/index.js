0a1f53a83cdd6352db393909d32a0bc99bcbdb84
jamesmillar <James@sailr.co.uk>
2023-10-23 17:33:51 +0100

    config swagger

--- VehicleService/src/index.js
+++ VehicleService/src/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 vehicleService.seed();
 
 // configure swagger
-app.get('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+require('./swagger')(app);
 
 // healthcheck endpoint
 /**
