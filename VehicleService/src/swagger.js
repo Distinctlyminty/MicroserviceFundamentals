9bd0120454020784487c10561a4db6192ae6754a
jamesmillar <James@sailr.co.uk>
2023-10-23 16:36:16 +0100

    Add prod server to swagger config

--- VehicleService/src/swagger.js
+++ VehicleService/src/swagger.js
@@ -11,7 +11,12 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000'
+        url: 'https://vehicleservice.azurewebsites.net',
+        description: 'Production server'
+      },
+      {
+        url: 'http://localhost:3000',
+        description: 'Local server'
       }
     ]
   },
